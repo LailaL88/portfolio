9462edd93a970703f92120662c33e77a6f01b086
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-05-28 22:05:53 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function smoothScroll() {
+    var element = document.querySelector(".profile");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function smoothScroll1() {
+    var element = document.querySelector(".projects");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function smoothScroll2() {
+    var element = document.querySelector(".contact");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
